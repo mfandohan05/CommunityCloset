9f9276b06f354a8d38d1e314ea5d7632adbb5445
Mereck Fandohan <fandohan@ad.unc.edu>
2025-11-19 22:09:10 -0500

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/CommunityCloset'>
     <StrictMode>
       <CartProvider>
         <App />
